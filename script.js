094d24083cd0fa75cd9ee8a1ca119c1fab50c6dc
infoclub-La-Nativite <al.stempfel@gmail.com>
2025-04-03 13:52:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-function paraph(){
-    window.open("gpsavion.html", "_self");
-}
+Tableau des cours
+const cours
+L
+f title: "Premier cours sur
+file: "mathematiques.pdf" ),
+.
+f title: "Deuxième cours", file: "physique.pdf" .
+[ title: "Troisième cours", file: "chimie.pdf" ),
+// Ajoutez vos cours ici
+]
 
-function paraph2(){
-    window.open("Maintenancepre.html", "_self");
-}
-function paraph3(){
-    window.open("Simulavion.html", "_self");
-}
 
-function opacity() {
-    var pourcent = (parseFloat(document.getElementById('1').value)/100);
-    console.log(pourcent);
-    var sections = document.getElementsByClassName("sectionv");
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.backgroundColor = "rgba(255, 255, 255, " + pourcent + ")";
+// Fonction pour générer la liste des cours
+function generateCoursList() f
+const coursList = document.getElementById("cours-list");
+cours.forEach((cours) >
+r
+const listItem = document.createElement("li");
+const link = document.createElement("a");
+link.href =`pdf/$fcours.file]`;
+link.textContent = cours.title;
+listItem.appendChild(link);
+coursList.appendChild(listItem);
+F
+F
 
-    }
-    document.getElementById('1').value = "";
-}
-document.getElementById('1').addEventListener('change', opacity)
+
+// Fonction pour afficher les actualités
+function displayActualites() [
+const actualitesContainer = document.getElementById("actualites-container");
+// Ajoutez vos actualités ici
+const actualitel = document.createElement("p");
+actualitel.textContent = "Actualité 1 ";
+actualitesContainer.appendChild(actualitel);
+const actualite2 = document.createElement("p");
+actualite2.textContent = "Actualité 2";
+actualitesContainer.appendChild(actualite2);
+// Ajoutez vos actualités ici
+
+
+// Appel des fonctions
+generateCoursList();
+displayActualites();
