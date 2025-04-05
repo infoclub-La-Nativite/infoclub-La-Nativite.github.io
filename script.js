70d42cae9e073dd6d7b35a6e23eb849c784d09d6
infoclub-La-Nativite <al.stempfel@gmail.com>
2025-04-05 22:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
+// Tableau des cours
 const cours = [
-  { title: "Premier cours", file : "mathematiques.pdf" },
-  { title: "Premier cours", file : "mathematiques.pdf" },
-  { title: "Premier cours", file : "mathematiques.pdf" },];
+    { title: "Premier cours sur ...", file: "mathematiques.pdf" },
+    { title: "Deuxième cours", file: "physique.pdf" },
+    { title: "Troisième cours", file: "chimie.pdf" },
+    // Ajoutez vos cours ici
+];
 
 // Fonction pour générer la liste des cours
-function generateCoursList(){
-  const coursList = document.getElementById("cours-list");
-  cours.forEach((cours) =>{
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.href =`pdf/$fcours.file]`;
-    link.textContent = cours.title;
-    listItem.appendChild(link);
-    coursList.appendChild(listItem);
-  });
+function generateCoursList() {
+    const coursList = document.getElementById("cours-list");
+    cours.forEach((cours) => {
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = `pdf/${cours.file}`;
+        link.textContent = cours.title;
+        listItem.appendChild(link);
+        coursList.appendChild(listItem);
+    });
 }
-
 
 // Fonction pour afficher les actualités
 function displayActualites() {
-  const actualitesContainer = document.getElementById("actualites-container");
-// Ajoutez vos actualités ici
-  const actualitel = document.createElement("p");
-  actualitel.textContent = "Actualité 1 ";
-  actualitesContainer.appendChild(actualitel);
-  const actualite2 = document.createElement("p");
-  actualite2.textContent = "Actualité 2";
-  actualitesContainer.appendChild(actualite2);
+    const actualitesContainer = document.getElementById("actualites-container");
+    // Ajoutez vos actualités ici
+    const actualite1 = document.createElement("p");
+    actualite1.textContent = "Actualité 1 ";
+    actualitesContainer.appendChild(actualite1);
+    const actualite2 = document.createElement("p");
+    actualite2.textContent = "Actualité 2";
+    actualitesContainer.appendChild(actualite2);
+    // Ajoutez vos actualités ici
 }
-// Ajoutez vos actualités ici
-
 
 // Appel des fonctions
 generateCoursList();
