67730db3445d199d1d9effa9cc9eb3974cfa0605
infoclub-La-Nativite <al.stempfel@gmail.com>
2025-04-05 21:34:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ const cours = [
   { title: "Premier cours", file : "mathematiques.pdf" },
   { title: "Premier cours", file : "mathematiques.pdf" },
   { title: "Premier cours", file : "mathematiques.pdf" },
-]
+];
 
 // Fonction pour générer la liste des cours
-function generateCoursList() f
-const coursList = document.getElementById("cours-list");
-cours.forEach((cours) >
-r
-const listItem = document.createElement("li");
-const link = document.createElement("a");
-link.href =`pdf/$fcours.file]`;
-link.textContent = cours.title;
-listItem.appendChild(link);
-coursList.appendChild(listItem);
-F
-F
+function generateCoursList(){
+  const coursList = document.getElementById("cours-list");
+  cours.forEach((cours) =>{
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href =`pdf/$fcours.file]`;
+    link.textContent = cours.title;
+    listItem.appendChild(link);
+    coursList.appendChild(listItem);
+    ]);
+}
 
 
 // Fonction pour afficher les actualités
-function displayActualites() [
-const actualitesContainer = document.getElementById("actualites-container");
+function displayActualites() {
+  const actualitesContainer = document.getElementById("actualites-container");
 // Ajoutez vos actualités ici
 const actualitel = document.createElement("p");
 actualitel.textContent = "Actualité 1 ";
